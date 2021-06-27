37db5f49835f0a03dec8b872d807a773e5c00a93
jcllobet <jcllobet@gmail.com>
2021-06-27 15:17:25 +0200

    added all rushes

--- rush04.c
+++ rush04.c
@@ -6,7 +6,7 @@
 /*   By: andrmart's group  <marvin@42.fr            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/26 09:28:24 by andrmart's group  #+#    #+#             */
-/*   Updated: 2021/06/27 01:14:06 by dpoveda-         ###   ########.fr       */
+/*   Updated: 2021/06/27 14:31:40 by dpoveda-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,21 @@ void	ft_putchar(char c);
 void	ft_checkpost_and_putchar(int c, int r, int i, int j)
 {
 	if (j == 0 && i == 0)
-		ft_putchar('/');
+		ft_putchar('A');
 	else if (j == c - 1 && i == 0)
-		ft_putchar('\\');
+		ft_putchar('C');
 	else if (j == 0 && i == r - 1)
-		ft_putchar('\\');
+		ft_putchar('C');
 	else if (j == c - 1 && i == r - 1)
-		ft_putchar('/');
+		ft_putchar('A');
 	else if (i == 0)
-		ft_putchar('*');
+		ft_putchar('B');
 	else if (i == r - 1)
-		ft_putchar('*');
+		ft_putchar('B');
 	else if (j == 0)
-		ft_putchar('*');
+		ft_putchar('B');
 	else if (j == c - 1)
-		ft_putchar('*');
+		ft_putchar('B');
 	else
 		ft_putchar(' ');
 }
